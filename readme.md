93c0ea675a922f1a7fc19e5a4beb689dce89af26
Sergiy Misyuk <smisyuk@gmail.com>
2023-04-11 00:17:16 +0300

    create readme file and add link to screenshot

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Module 1 - Install
+![App Screenshot](https://i.ibb.co/99PLhrf/2023-04-10-23-47-00.png)
+
---
+++ src/componets/Nav.js
@@ -0,0 +1,18 @@
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export const Nav = () => {
+//     return (
+//         <View style={styles.container}>
+//             <Text>My pain</Text>
+//         </View>
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#25292e',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//   });
