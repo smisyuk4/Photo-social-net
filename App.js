4dc4803065b7eeff374b4c2e9de953cdd157e30f
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-05-22 21:04:12 +0300

    ADD: check user on App for routing

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
+import { auth } from './firebase/config';
+import { onAuthStateChanged } from 'firebase/auth';
 import { useRoute } from './src/routes';
 import { LoaderScreen } from './src/Screens/LoaderScreen';
 
@@ -10,7 +13,11 @@ export const App = () => {
     'Roboto-Italic': require('./src/fonts/Roboto/Roboto-Italic.ttf'),
     'Roboto-Regular': require('./src/fonts/Roboto/Roboto-Regular.ttf'),
   });
-  const routing = useRoute(true);
+  const [user, setUser] = useState(null);
+
+  onAuthStateChanged(auth, user => setUser(user));
+
+  const routing = useRoute(user);
 
   if (!fontsLoaded) {
     return <LoaderScreen />;
